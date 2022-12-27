9e4542f2be9eb5d28aaaf6be8e5299003d1a9756
olawanle temitayo <olawanletemitayo@gmail.com>
2022-12-27 14:14:16 +0100

    done with the gallery page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import { excoData, facultyData, orientationData } from '../../components/GalleryData';
+import styles from './page.module.css';
 
 const page = () => {
   return (
-    <div></div>
-  )
-}
+    <div className={styles.wrapper}>
+      <div className={styles.inner}>
+        <header className={styles.header}>
+          <h1>Gallery</h1>
+        </header>
+        <div className={styles.content}>
+          <div className={styles.faculty__gallery}>
+            <h1>Faculty of Management Sciences</h1>
+            <p>
+              “Everything has beauty, but not everyone sees it.” - Confucius
+            </p>
+            <div className={styles.gallery__ctn}>
+              {facultyData.map((item, index) => {
+                return (
+                  <div className={styles.gallery__img} key={index}>
+                    <Image
+                      src={item.src}
+                      alt="images"
+                      height={500}
+                      width={400}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className={styles.exco__gallery}>
+            <h1>MSA Excos Inauguration Day</h1>
+            <p>
+              “Leadership is the capacity to translate vision into reality.”
+              —Warren Bennis
+            </p>
+            <div className={styles.gallery__ctn}>
+              {excoData.map((item, index) => {
+                return (
+                  <div className={styles.gallery__img} key={index}>
+                    <Image
+                      src={item.src}
+                      alt="images"
+                      height={500}
+                      width={400}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className={styles.exco__gallery}>
+            <h1>MSA Orientation Day</h1>
+            <p>
+              “Orientation is the process of acclimatizing freshers to the new
+              environment and the new culture of the university. It is a process
+              of familiarizing the freshers with the university and its
+              environment.” — Dr. A. K. Singh
+            </p>
+            <div className={styles.gallery__ctn}>
+              {orientationData.map((item, index) => {
+                return (
+                  <div className={styles.gallery__img} key={index}>
+                    <Image
+                      src={item.src}
+                      alt="images"
+                      height={500}
+                      width={400}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
---
+++ components/GalleryData.ts
@@ -0,0 +1,105 @@
+interface IFaculty {
+  src: string;
+}
+
+export const facultyData: IFaculty[] = [
+  {
+    src: '/images/faculty-images/faculty-banner.jpg',
+  },
+  {
+    src: '/images/faculty-images/faculty-reception.jpg',
+  },
+  {
+    src: '/images/faculty-images/faculty-reception2.jpg',
+  },
+  {
+    src: '/images/faculty-images/faculty-outside-view.jpg',
+  },
+  {
+    src: '/images/faculty-images/faculty-outside-view2.jpg',
+  },
+  {
+    src: '/images/faculty-images/faculty-outside-view3.jpg',
+  },
+  {
+    src: '/images/faculty-images/faculty-outside-view4.jpg',
+  },
+];
+
+export const excoData: IFaculty[] = [
+  {
+    src: '/images/exco-images/exco-1.jpg',
+  },
+  {
+    src: '/images/exco-images/exco-2.jpg',
+  },
+  {
+    src: '/images/exco-images/exco-3.jpg',
+  },
+  {
+    src: '/images/exco-images/exco-4.jpg',
+  },
+  {
+    src: '/images/exco-images/exco-5.jpg',
+  },
+  {
+    src: '/images/exco-images/exco-6.jpg',
+  },
+  {
+    src: '/images/exco-images/exco-7.jpg',
+  },
+  {
+    src: '/images/exco-images/exco-8.jpg',
+  },
+  {
+    src: '/images/exco-images/exco-9.jpg',
+  },
+];
+
+export const orientationData: IFaculty[] = [
+  {
+    src: '/images/orientation/orientation-1.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-2.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-3.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-4.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-5.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-6.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-7.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-8.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-9.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-10.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-11.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-12.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-13.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-14.jpg',
+  },
+  {
+    src: '/images/orientation/orientation-15.jpg',
+  },
+];
