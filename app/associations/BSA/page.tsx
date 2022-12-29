992ebce60b55dc5418d4642e85f9bc7ff303d4c5
olawanle temitayo <olawanletemitayo@gmail.com>
2022-12-29 17:26:16 +0100

    feat: built the ERHRMSA page

--- app/associations/BSA/page.tsx
+++ app/associations/BSA/page.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { bsaData } from '../../../components/BsaData';
+import TeamCard from '../../TeamCard';
+import styles from './page.module.css'
 
 const page = () => {
   return (
-    <div>page</div>
-  )
+    <div className={styles.wrapper}>
+      <div className={styles.banner}>
+        <div className={styles.bannerText}>
+          <h1>BSA</h1>
+          <p>Business Students Association</p>
+        </div>
+      </div>
+      <main className={styles.main__content}>
+        <header className={styles.header}>
+          <h1>Our Team</h1>
+          <p>BSA EXECUTIVES 21/22 (THE SYNERGY)</p>
+        </header>
+        <div className={styles.team__container}>
+          {bsaData.map((data) => {
+            return <TeamCard data={data} key={data.name} />;
+          })}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default page
--- app/associations/ERHRMSA/page.tsx
+++ app/associations/ERHRMSA/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { erhrmsaData } from '../../../components/ErhrmsaData';
+import TeamCard from '../../TeamCard';
+import styles from './page.module.css';
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className={styles.wrapper}>
+      <div className={styles.banner}>
+        <div className={styles.bannerText}>
+          <h1>ER&HRMSA</h1>
+          <p>
+            Employee Relations and Human Resources <br /> Management Student
+            Association
+          </p>
+        </div>
+      </div>
+      <main className={styles.main__content}>
+        <header className={styles.header}>
+          <h1>Our Team</h1>
+          <p>ER&HRMSA EXECUTIVES 21/22 (THE SYNERGY)</p>
+        </header>
+        <div className={styles.team__container}>
+          {erhrmsaData.map((data) => {
+            return <TeamCard data={data} key={data.name} />;
+          })}
+        </div>
+      </main>
+    </div>
+  );
+};
 
-export default page
+export default page;
